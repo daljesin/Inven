66cfdffcd61e12fe16e9d63ff8051f582fcc7afb
anksha <anil2008.com@gmail.com>
2017-02-25 15:07:20 +0530

    commit2

---
+++ include/topmenu.php
@@ -0,0 +1,154 @@
+<div class="nav-collapse collapse navbar-collapse navbar-responsive-collapse">
+                            <ul class="nav navbar-nav">
+                                <li class="dropdown dropdown-fw dropdown-fw-disabled  active open selected">
+                                    <a href="javascript:;" class="text-uppercase">
+                                        <i class="icon-home"></i> Dashboard </a>
+                                    <!--<ul class="dropdown-menu dropdown-menu-fw">
+                                        <li>
+                                            <a href="index.html">
+                                                <i class="icon-bar-chart"></i> Default </a>
+                                        </li>
+                                       
+                                    </ul>-->
+                                </li>
+                                <li class="dropdown dropdown-fw dropdown-fw-disabled  ">
+                                    <a href="javascript:;" class="text-uppercase">
+                                        <i class="icon-puzzle"></i> Inventory </a>
+                                    <ul class="dropdown-menu dropdown-menu-fw">
+                                        <li class="dropdown">
+                                            <a href="javascript:;">
+                                                <i class="icon-diamond"></i>Purchase</a>
+                                            <!--<ul class="dropdown-menu">
+                                                <li>
+                                                    <a href="ui_colors.html">Stock</a>
+                                                </li>
+                                                <li>
+                                                    <a href="ui_metronic_grid.html">Category</a>
+                                                </li>
+                                                <li>
+                                                    <a href="ui_general.html">Product List</a>
+                                                </li>
+                                               
+                                            </ul>-->
+                                        </li>
+                                        <li class="dropdown">
+                                            <a href="javascript:;">
+                                                <i class="icon-puzzle"></i>Stock</a>
+                                           
+                                        </li>
+                                        <li class="dropdown">
+                                            <a href="javascript:;">
+                                                <i class="icon-settings"></i>Category</a>
+                                        </li>
+                                        <li class="dropdown">
+                                            <a href="javascript:;">
+                                                <i class="icon-bar-chart"></i>Product List</a>
+                                            
+                                        </li>
+                                        
+                                   
+                                    </ul>
+                                </li>
+                                <li class="dropdown dropdown-fw dropdown-fw-disabled  ">
+                                    <a href="javascript:;" class="text-uppercase">
+                                        <i class="icon-briefcase"></i>Sales</a>
+                                    <ul class="dropdown-menu dropdown-menu-fw">
+                                        <li>
+                                            <a href="table_static_basic.html">Quote</a>
+                                        </li>
+                                        <li>
+                                            <a href="table_static_responsive.html">Sales Order </a>
+                                        </li>
+                                        <li>
+                                            <a href="table_bootstrap.html">Sales Invoice </a>
+                                        </li>
+										 <li>
+                                            <a href="table_bootstrap.html">Sales Vouchers </a>
+                                        </li>
+                                      
+                                    </ul>
+                                </li>
+                                <li class="dropdown dropdown-fw dropdown-fw-disabled  ">
+                                    <a href="javascript:;" class="text-uppercase">
+                                        <i class="icon-layers"></i>Payments </a>
+                                    <ul class="dropdown-menu dropdown-menu-fw">
+                                        <li class="dropdown">
+                                            <a href="javascript:;">
+                                                <i class="icon-basket"></i>Sales Vouchers </a>
+                                        </li>
+                                       
+                                        <li class="dropdown">
+                                            <a href="javascript:;">
+                                                <i class="icon-user"></i>Purchase Vouchers </a>
+                                           
+                                        </li>
+                                        <li class="dropdown">
+                                            <a href="javascript:;">
+                                                <i class="icon-social-dribbble"></i>Expense Vouchers</a>
+                                        </li>
+                                        <li class="dropdown">
+                                            <a href="javascript:;">
+                                                <i class="icon-settings"></i> Make Payment  </a>
+                                           
+                                        </li>
+										<li class="dropdown">
+                                            <a href="javascript:;">
+                                                <i class="icon-settings"></i> Payment History  </a>
+                                           
+                                        </li>
+                                    </ul>
+                                </li>
+								<li class="dropdown dropdown-fw dropdown-fw-disabled  ">
+                                    <a href="javascript:;" class="text-uppercase">
+                                        <i class="icon-layers"></i>Master </a>
+                                    <ul class="dropdown-menu dropdown-menu-fw">
+                                        <li class="dropdown">
+                                            <a href="javascript:;">
+                                                <i class="icon-basket"></i>Customers </a>
+                                        </li>
+                                       
+                                        <li class="dropdown">
+                                            <a href="javascript:;">
+                                                <i class="icon-user"></i>Vendors </a>
+                                           
+                                        </li>
+                                        <li class="dropdown">
+                                            <a href="javascript:;">
+                                                <i class="icon-social-dribbble"></i>Users</a>
+                                        </li>
+                                        <li class="dropdown">
+                                            <a href="addstore.php">
+                                                <i class="icon-settings"></i>Stores</a>
+                                           
+                                        </li>
+										
+                                    </ul>
+                                </li>
+                                <!--<li class="dropdown more-dropdown">
+                                    <a href="javascript:;" class="text-uppercase">Master</a>
+									 <ul class="dropdown-menu dropdown-menu-fw">
+                                        <li>
+                                            <a href="table_static_basic.html">Customers</a>
+                                        </li>
+                                        <li>
+                                            <a href="table_static_responsive.html">Vendors</a>
+                                        </li>
+                                        <li>
+                                            <a href="table_bootstrap.html">Users</a>
+                                        </li>
+										 <li>
+                                            <a href="table_bootstrap.html">Stores </a>
+                                        </li>
+                                      
+                                    </ul>
+                                 </li>-->  
+								  <li class="dropdown more-dropdown">
+                                    <a href="javascript:;" class="text-uppercase">Reports</a>
+									
+                                 </li>  
+								  <li class="dropdown more-dropdown">
+                                    <a href="javascript:;" class="text-uppercase">Settings</a>
+									
+                                 </li>  
+                            </ul>
+                        </div>
